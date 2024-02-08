6c48246795228ebc3fccd8344e4afdee14ddf32a
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-02-08 16:05:51 +0000

    Commit 3 (Daily)

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ let engine = new Coffee();
 engine.createWindow(320, 240);
 engine.displayWindow();
 
-let morshu_image = new CoffeeSprite('morshu', 128, 128);
+let morshu_image = new CoffeeObject();
+morshu_image.sprite = new CoffeeSprite('morshu', 128, 64);
+morshu_image.scaleTo(128, 128);
 
 setInterval(() => {
     engine.wipe();
     engine.fillRect(0, 0, 320, 240);
-    engine.drawSprite(morshu_image, 0, 0);
+    engine.drawObject(morshu_image);
 }, 0);
