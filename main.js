37b73c4c607ed135e42a1f54355c1c8493e475f6
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-02-09 15:50:00 +0000

    Commit 4 (Daily)

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 let engine = new Coffee();
 
-engine.createWindow(320, 240);
+engine.createWindow(320,240);
+engine.windowScale = 1;
 engine.displayWindow();
 
-let morshu_image = new CoffeeObject();
-morshu_image.sprite = new CoffeeSprite('morshu', 128, 64);
-morshu_image.scaleTo(128, 128);
+let player = new CoffeeObject();
+player.goTo(engine.origin_x, engine.origin_y);
+
+let angle = 0;
 
 setInterval(() => {
+    if (CoffeeKeys.ArrowLeft) {
+        angle--;
+    }
+    if (CoffeeKeys.ArrowRight) {
+        angle++;
+    }
     engine.wipe();
-    engine.fillRect(0, 0, 320, 240);
-    engine.drawObject(morshu_image);
+    engine.fillColor = 'black';
+    engine.fillRect(0, 0, engine.window.width, engine.window.height);
+    engine.fillColor = 'white';
+    engine.fillRect(player.x-3, player.y-3, 6, 6);
+    engine.lineColor = 'red';
+    engine.lineWidth = 1;
+    engine.angledLine(player.x, player.y, rad(angle), 8);
 }, 0);
