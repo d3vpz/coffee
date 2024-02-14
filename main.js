88dbfa49f1a7de4d0672033f462a9fea0fcc7dc0
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-02-14 17:36:20 +0000

    Project template setup

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
 let engine = new Coffee();
 
-engine.createWindow(320,240);
-engine.windowScale = 1;
+engine.createWindow();
 engine.displayWindow();
 
-let player = new CoffeeObject();
-player.goTo(engine.origin_x, engine.origin_y);
-
-let angle = 0;
-
-setInterval(() => {
-    if (CoffeeKeys.ArrowLeft) {
-        angle--;
-    }
-    if (CoffeeKeys.ArrowRight) {
-        angle++;
-    }
-    engine.wipe();
-    engine.fillColor = 'black';
-    engine.fillRect(0, 0, engine.window.width, engine.window.height);
-    engine.fillColor = 'white';
-    engine.fillRect(player.x-3, player.y-3, 6, 6);
-    engine.lineColor = 'red';
-    engine.lineWidth = 1;
-    engine.angledLine(player.x, player.y, rad(angle), 8);
-}, 0);
+engine.fillColor = "black";
+engine.fillRect(0, 0, 320, 240);
