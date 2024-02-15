89572b5ef12386a40c84f745d97b77b47d32616c
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-02-15 15:01:56 +0000

    update

--- coffee_update.sh
+++ coffee_update.sh
@@ -4,6 +4,10 @@ cd lib
 wget https://raw.githubusercontent.com/d3vpz/coffee/main/lib/coffee_main.js -O coffee_main.js
 wget https://raw.githubusercontent.com/d3vpz/coffee/main/lib/coffee_math.js -O coffee_math.js
 
+cd ..
+
+wget https://raw.githubusercontent.com/d3vpz/coffee/main/coffee_update.sh -O coffee_update.sh
+
 BOLD=$(tput bold)
 GREEN='\033[1;32m'
-echo "${GREEN}Coffee has been updates successfully.\n"
+echo "${GREEN}Coffee has been updated successfully.\n"
