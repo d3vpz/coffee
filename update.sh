825d34bbd20eeb1bb13d2d278212c3ee131a197e
Devpz <114014131+d3vpz@users.noreply.github.com>
2024-02-23 18:25:58 +0000

    remove README when updating project

--- update.sh
+++ update.sh
@@ -10,5 +10,7 @@ cd ..
 wget https://raw.githubusercontent.com/d3vpz/coffee/main/update.sh -O update.sh
 wget https://raw.githubusercontent.com/d3vpz/coffee/main/package.sh -O package.sh
 
+rm README.md
+
 GREEN='\033[1;32m'
 echo "${GREEN}Coffee has been updated successfully.\n"
